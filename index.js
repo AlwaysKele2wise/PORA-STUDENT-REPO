4de508504052b2d6b4063c308d8e1c2e28408d27
AlwaysKele2wise <kele2wise@gmail.com>
2024-04-19 15:04:33 +0100

    add working

--- index.js
+++ index.js
@@ -6,9 +6,29 @@
 
 // dev()
 
-
-const dev = (par1,par2) =>{
-console.log("dev")
+/*const dev = (par1,par2) => {
+console.log({sum:par1+par2})
+console.log("Kelechi ukpai")
 }
 
-dev(2, 3)
+dev(2, 3)*/
+
+function toCelsius(f) {
+    return (f - 32) * 5 / 9
+}
+
+function toFahrenheit(c) {
+    return (c * 9) / 5 + 32
+}
+function Weight(Pounds){
+    return 7 * 2.2
+}
+function distance () {
+    return 7 * 0.62137
+}
+
+console.log(toCelsius(77))
+console.log(toFahrenheit(100))
+console.log(Weight(1000))
+console.log(distance())
+
